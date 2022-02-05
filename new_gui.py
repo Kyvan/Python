bcee5abcd8a9813c6f053e0210bdca19ee039ff9
Kyvan Emami <emamikyvan@gmail.com>
2022-02-05 08:43:22 -0500

    Updated to have random order of words

--- new_gui.py
+++ new_gui.py
@@ -38,10 +38,20 @@ class MyWindow(QtWidgets.QWidget):
                         word2 = rw.random_word(letter=None)
                         pass_phrase(word1, word2)
 
-
                 def pass_phrase(_word1, _word2):
-                        print (f'{_word1.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}'
-                        f'{_word2.capitalize()}{randint(1, 9)}')
+                        pass1 = f'{_word1.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}{_word2.capitalize()}{randint(1, 9)}'
+                        pass2 = f'{randint(1, 9)}{_word1.capitalize()}{random.choice(string.punctuation)}{_word2.capitalize()}{randint(1, 9)}'
+                        pass3 = f'{random.choice(string.punctuation)}{_word1.capitalize()}{randint(1, 9)}{_word2.capitalize()}{randint(1, 9)}'
+                        pass4 = f'{_word1.capitalize()}{random.choice(string.punctuation)}{randint(1, 9)}{_word2.capitalize()}{randint(1, 9)}'
+                        pass5 = f'{_word1.capitalize()}{random.choice(string.punctuation)}{_word2.capitalize()}{randint(1, 9)}{randint(1, 9)}'
+                        pass6 = f'{_word1.capitalize()}{randint(1, 9)}{_word2.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}'
+                        pass7 = f'{_word1.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}{randint(1, 9)}{_word2.capitalize()}'
+                        pass8 = f'{_word1.capitalize()}{randint(1, 9)}{randint(1, 9)}{random.choice(string.punctuation)}{_word2.capitalize()}'
+                        pass9 = f'{_word1.capitalize()}{_word2.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}{randint(1, 9)}'
+
+                        passwords = [pass1, pass2, pass3, pass4, pass5, pass6, pass7, pass8, pass9]
+                        print (random.choice(passwords))
+
 
 
                 for x in range(5):
--- passgen.py
+++ passgen.py
@@ -1,8 +1,8 @@
 from random_words import RandomWords
 from random import randint
-import sys
-import string
 import random
+import string
+import sys
 
 rw = RandomWords()
 
@@ -14,8 +14,18 @@ def word_generator():
 
 
 def pass_phrase(_word1, _word2):
-   print (f'{_word1.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}\
-{_word2.capitalize()}{randint(1, 9)}')
+   pass1 = f'{_word1.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}{_word2.capitalize()}{randint(1, 9)}'
+   pass2 = f'{randint(1, 9)}{_word1.capitalize()}{random.choice(string.punctuation)}{_word2.capitalize()}{randint(1, 9)}'
+   pass3 = f'{random.choice(string.punctuation)}{_word1.capitalize()}{randint(1, 9)}{_word2.capitalize()}{randint(1, 9)}'
+   pass4 = f'{_word1.capitalize()}{random.choice(string.punctuation)}{randint(1, 9)}{_word2.capitalize()}{randint(1, 9)}'
+   pass5 = f'{_word1.capitalize()}{random.choice(string.punctuation)}{_word2.capitalize()}{randint(1, 9)}{randint(1, 9)}'
+   pass6 = f'{_word1.capitalize()}{randint(1, 9)}{_word2.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}'
+   pass7 = f'{_word1.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}{randint(1, 9)}{_word2.capitalize()}'
+   pass8 = f'{_word1.capitalize()}{randint(1, 9)}{randint(1, 9)}{random.choice(string.punctuation)}{_word2.capitalize()}'
+   pass9 = f'{_word1.capitalize()}{_word2.capitalize()}{randint(1, 9)}{random.choice(string.punctuation)}{randint(1, 9)}'
+
+   passwords = [pass1, pass2, pass3, pass4, pass5, pass6, pass7, pass8, pass9]
+   print (random.choice(passwords))
 
 
 if len(sys.argv) == 2:
