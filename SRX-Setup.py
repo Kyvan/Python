843bcbf902cf3416080d44d22135c56391298a8c
Kyvan Emami <emamikyvan@gmail.com>
2024-03-05 21:13:28 -0500

    new script and updated formatting

---
+++ SRX-Setup.py
@@ -0,0 +1,81 @@
+# Libraries to imprt
+import re
+
+# Variables to be used later on
+unitNumb = 0
+vlanID1 = 4
+vlanID2 = 1
+firstInterface = 18
+secondInterface = 19
+aeInterface = "ae0"
+
+# Asking for Gateway Address
+defaultGW = input("What is the Default Gateway? ") 
+nativeVLAN = input("What is the Native VLAN? ") 
+
+
+# def to print ERROR messages
+def error(self):
+    print("ERROR: " + defaultGW + " not a valid IP!!")
+    exit()
+
+
+def ipchecker(arg1):
+    # Checks for validfity of the IP based on the below regex comparison
+    gateway = re.match(r"(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}\.){3}(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})", arg1)
+
+    # Errors out if it not a valid IP
+    if not bool(gateway):
+        error(arg1)
+
+
+ipchecker(defaultGW)
+
+# Commands needed for setting up the " + str(aeInterface)" interface
+print("set interfaxes xe-0/0/" + str(firstInterface) + " gigether-options 802.3ad " + str(aeInterface))
+print("set interfaxes xe-0/0/" + str(secondInterface) + " gigether-options 802.3ad " + str(aeInterface))
+print("set interfaces " + str(aeInterface) + " flexible-vlan-tagging")
+print("set interfaces " + str(aeInterface) + " native-vlan-id " + nativeVLAN)
+print("set interfaces " + str(aeInterface) + " aggregated-ether-options link-speed 10g")
+print("set interfaces " + str(aeInterface) + " aggregated-ether-options lacp active")
+
+# Not sure why this line is needed, but things didn't work propery without it
+print("set chassis aggregated-devices ethernet device-count 44")
+
+# Default route to be added
+print("set routing-options static route 0.0.0.0/0 next-hop " + defaultGW)
+
+# While loop to create sub interfaces for each VLAN
+while vlanID1 < 9:
+    while vlanID2 < 9:    
+        print("set interfaces " + str(aeInterface) + " unit " + str(unitNumb) + " description " + str(vlanID1) + "0" + str(vlanID2))
+        print("set interfaces " + str(aeInterface) + " unit " + str(unitNumb) + " vlan-id " + str(vlanID1) + "0" + str(vlanID2))
+        print("set interfaces " + str(aeInterface) + " unit " + str(unitNumb) + " family inet address 10." + str(vlanID1) + str(vlanID2) + ".0.1/24")
+        print("set forwarding-options dhcp-relay relay-option-82 circuit-id use-interface-description device")
+        print("set forwarding-options dhcp-relay forward-only")
+        print("set forwarding-options dhcp-relay server-group " + str(vlanID1) + "0" + str(vlanID2) + "-serverGroup 10.30.0.2")
+        print("set forwarding-options dhcp-relay active-server-group " + str(vlanID1) + "0" + str(vlanID2) + "-serverGroup")
+        print("set forwarding-options dhcp-relay group " + str(vlanID1) + "0" + str(vlanID2) + "-serverGroup interface " + str(aeInterface) + "." + str(unitNumb))
+        print("set security zones security-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " description " + str(vlanID1) + "0" + str(vlanID2))
+        print("set security zones security-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " address-book address VLAN" + str(vlanID1) + "0" + str(vlanID2) + " 10." + str(vlanID1) + str(vlanID2) + ".0.0/24")
+        print("set security zones security-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " interfaces " + str(aeInterface) + "." + str(unitNumb) + " host-inbound-traffic system-services snmp")
+        print("set security zones security-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " interfaces " + str(aeInterface) + "." + str(unitNumb) + " host-inbound-traffic system-services ping")
+        print("set security zones security-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " interfaces " + str(aeInterface) + "." + str(unitNumb) + " host-inbound-traffic system-services traceroute")
+        print("set security zones security-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " interfaces " + str(aeInterface) + "." + str(unitNumb) + " host-inbound-traffic system-services dhcp")
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT from zone VLAN" + str(vlanID1) + "0" + str(vlanID2))
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT to zone Beanfield")
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT rule VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-OUT match source-address 10." + str(vlanID1) + str(vlanID2) + ".0.0/24")
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT rule VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-OUT match destination-address 0.0.0.0/0")
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT rule VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-OUT match protocol tcp")
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT rule VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-OUT match protocol udp")
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT rule VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-OUT match protocol icmp")      
+        print("set security nat source rule-set VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-NAT rule VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-OUT then source-nat pool Beanfield")
+        print("set security policies from-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " to-zone Beanfield policy VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-Out match source-address VLAN" + str(vlanID1) + "0" + str(vlanID2))
+        print("set security policies from-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " to-zone Beanfield policy VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-Out match destination-address any")
+        print("set security policies from-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " to-zone Beanfield policy VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-Out match application any")
+        print("set security policies from-zone VLAN" + str(vlanID1) + "0" + str(vlanID2) + " to-zone Beanfield policy VLAN" + str(vlanID1) + "0" + str(vlanID2) + "-Out then permit")
+        vlanID2 += 1
+        unitNumb += 1
+    vlanID2 = 1
+    vlanID1 += 1
+    
--- game.py
+++ game.py
@@ -5,11 +5,11 @@ Weapon = "By taking this weapon, you have +50 damage now. Maybe it will be usefu
 Armor = "By taking this Armor, you now have +10 defense. Might be useful in the near future!!"
 Potion = "This potion gives you +5 damage boost!!"
 Book = "Now that you have read this book, you know more about defending. +5 to defense!!!!"
-Script = "Now you know how to script, so go ahead and make your own game if you don't like mine.\nYour health is also replenished."
+Script = "Now you know how to script, so go ahead and make your own game if you don't like mine."
 
 # Variables to be used in Room generation.
 roomNums = [1, 2]
-roomNumber = 0
+roomnumber = 0
 roomObj = ["Weapon", "Armor", "Potion", "Book", "Script"]
 
 # Variables to keep track of player's choices, health, and damage output.
@@ -20,108 +20,124 @@ playerArmor = []
 playerPotion = []
 playerBook = []
 playerHealth = 450
-playerDmg = 150 + randrange(0,50)
+playerDmg = 150 + randrange(0, 50)
 weaponDmg = int(len(playerWeapon) * 15)
 armorBlock = int(len(playerArmor) * 15)
-block = int(playerHealth) + randrange(0,50) + int(armorBlock) + int(len(playerBook) * 5)
+block = int(playerHealth) + randrange(0, 50) + int(armorBlock) + int(len(playerBook) * 5)
 
 # Variables to be used by the Final Boss and Random enemies in rooms.
-bossDmg = randrange(300,500)
+bossDmg = randrange(300, 500)
 bossHealth = 275
 monsterDmg = 200
-monsterHealth = randrange(100,201)
+monsterHealth = randrange(100, 201)
+
+
+# Room function to generate the rooms the player is going to be in.
+def room():
+    roomnumber = choice(roomNums)
+    if roomnumber == 2:
+        print("Congratulation, you have made it to room number 10.")
+        print("You have the chance to attack the Boss and win the game.")
+        print("The Boss has 300 health.")
+        bossfight = input("Would you like to attack the boss? ")
+        if bossfight.lower() == "yes" or bossfight.lower() == "y":
+            dmg = int(playerDmg) + int(len(playerWeapon) * weaponDmg) + int(len(playerPotion) * 5)
+            if dmg > bossHealth:
+                print("Congratulations!!!! You did " + str(
+                    dmg) + " damage to the final boss and killed it. You have finished the game like a badass you are")
+            elif int(block / 2) > bossDmg:
+                print("The final boos attacked you before you had a chance, but you surviced with " + str(
+                    block - bossDmg) + " health got out of the room.\nCongratulations on finishing the game.")
+            else:
+                print(
+                    "The final boss attacked you before you had a chance and killed you!!!")
+        else:
+            print("I see you chose to be a chicken and not fight the final boss.")
+            print("The final boss didn't care, so he attacked and killed you anyways!!!")
+        print("These are the choices you made while playing the game: " + str(playerChoice))
+    elif roomnumber % 2 == 0:
+        monster()
+    else:
+        print("You have entered room number: " + str(roomnumber))
+        itemchecker()
+        answercheck()
+
 
 """
 Function to show the rules of the game.
 What's the game about you might ask, none of your business is the answer!!!!!!
 Now fuck off and play the damn game.
-"""			
-def rules():
-	print ("These are the rules of the game, at any prompt, you can type \"rules\" to see them again.")
-	print ("1. There are 10 rooms and you could start in any room you happen to spawn in.")
-	print ("2. You have the choice of going to \"Right\" or \"Up\" in each room.")
-	print ("3. Some rooms have items in them, you can interact with them by typing the name of the item you want.")
-	print ("4. By typing anything besides the stuff mentioned above, you can quit the game.")
-	print ("5. After reaching room number 10, the game is over and you will win if you manage to defeat the boss in that room\n\n")
-	room()
+"""
 
-#Room function to generate the rooms the player is going to be in.
-def room():
-	roomNumber = choice(roomNums)
-	if roomNumber == 2:
-		print ("Congratulation, you have made it to room number 10.")
-		print ("You have the chance to attack the Boss and win the game.")
-		print ("The Boss has 300 health.")
-		bossFight = input("Would you like to attack the boss? ")
-		if bossFight.lower() == "yes" or bossFight.lower() == "y":
-			dmg = int(playerDmg)  + int(len(playerWeapon) * weaponDmg) + int(len(playerPotion) * 5)
-			if dmg > bossHealth:
-				print ("Congratulations!!!! You did " + str(dmg) + " damage to the final boss and killed it. You have finished the game like a badass you are!!!!")
-			elif int(block / 2) > bossDmg:
-				print ("The final boos attacked you before you had a chance, but you surviced with " + str(block - bossDmg) + " health and managed to get out of the room.\nCongratulations on finishing the game.")
-			else:
-				print ("The final boss attacked you before you had a chance and killed you.\nNot sure why I'm telling you this considering that you are dead!!")
-		else:
-			print ("I see you chose to be a chicken and not fight the final boss.\nThe final boss didn't care tho, so he attacked and killed you anyways!!!")
-		print ("These are the choices you made while playing the game: " + str(playerChoice))
-	elif roomNumber % 2 == 0:
-		monster()
-	else:
-		print ("You have entered room number: " + str(roomNumber))
-		Checker().itemChecker()
-		Checker().answerCheck()
-		
+
+def rules():
+    print("These are the rules of the game, at any prompt, you can type \"rules\" to see them again.")
+    print("1. There are 10 rooms and you could start in any room you happen to spawn in.")
+    print("2. You have the choice of going to \"Right\" or \"Up\" in each room.")
+    print("3. Some rooms have items in them, you can interact with them by typing the name of the item you want.")
+    print("4. By typing anything besides the stuff mentioned above, you can quit the game.")
+    print("5. After reaching room number 10, the game is over.")
+    print("You will win if you manage to defeat the boss in that room\n\n")
+    room()
+
+
 # Function to generate a random monster to be used in a random room.
 def monster():
-	print ("You have entered room number: " + str(roomNumber))
-	print ("There seems to be a monster in this room. You HAVE to fight it in order to be able to intract with the items in the room!!")
-	monsterFight = input("So, do you wanna attack it or just move on??? ")
-	if monsterFight.lower() == "yes" or monsterFight.lower() == "y":
-		dmg = int(playerDmg)  + int(len(playerWeapon) * weaponDmg) + int(len(playerPotion) * 5)
-		if dmg >= monsterHealth:
-			print ("Congratulations!!!! You did " + str(dmg) + " damage to the monster and killed it.")
-			Checker().itemChecker()
-			Checker().answerCheck()
-		elif int(block / 2) >= monsterDmg:   
-			print ("The monster attacked you before you had a chance, but you surviced with " + str(block - monsterDmg) + " and managed to get away from it. Quickly, " + room())
-		else:
-			print ("The monster attacked you before you had a chance and killed you.\nNot sure why I'm telling you this considering that you are dead!!")
-	else:
-		print ("I see you chose to be a chicken and not fight the monster.\nThe monster didn't care and attacked you anyways!!!")
-		print ("You got lucky tho and manged to get to the next room, you might not be this lucky next time.\n")
-		room()
+    print("You have entered room number: " + str(roomnumber))
+    print("There seems to be a monster in this room. You must fight it so you can intract with the items in the room!!")
+    monsterfight = input("So, do you wanna attack it or just move on??? ")
+    if monsterfight.lower() == "yes" or monsterfight.lower() == "y":
+        dmg = int(playerDmg) + int(len(playerWeapon) * weaponDmg) + int(len(playerPotion) * 5)
+        if dmg >= monsterHealth:
+            print("Congratulations!!!! You did " + str(dmg) + " damage to the monster and killed it.")
+            itemchecker()
+            answercheck()
+        elif int(block / 2) >= monsterDmg:
+            print("The monster attacked you before you had a chance, but you surviced with " + str(
+                block - monsterDmg) + " and managed to get away from it. Quickly, ")
+            room()
+        else:
+            print("The monster attacked you before you had a chance and killed you.")
+            print("Not sure why I'm telling you this considering that you are dead!!")
+    else:
+        print("I see you chose to be a chicken and not fight the monster.")
+        print("The monster didn't care and attacked you anyways!!!")
+        print("You got lucky tho and manged to get to the next room, you might not be this lucky next time.\n")
+        room()
 
-#Checker class to check the user's answer.
-class Checker:
-	
-	# Function to check and see what direction the user wants to go to. 
-	def answerCheck(self):
-		answer = input("Where do you wanna go? ")
-		playerChoice.append(answer)
-		if answer.lower() == "rules":
-			rules()
-		elif answer.lower() == "up" or answer.lower() == "u" or answer.lower() == "right" or answer.lower() == "r":
-			room()
-		else:
-			print ("I guess you missed the instructions!!!!\nYou won't get a chance to fix this issue, so you can start from scratch if you want")
-	
-	# Function to check what item the user wants to interact with.
-	def itemChecker(self):
-		obj = input("Which one of these items " + str(sample(roomObj, 2)) + " whould you like to interact with? ")
-		if obj.lower() == "weapon":
-			print (Weapon + "\n")
-		elif obj.lower() == "armor":
-			print (Armor + "\n")
-		elif obj.lower() == "potion":
-			print (Potion + "\n")
-			playerPotion.append(obj.lower())
-		elif obj.lower() == "book":
-			print (Book +  "\n")
-			playerBook.append(obj.lower())
-		elif obj.lower() == "script":
-			print (Script + "\n")
-		else:
-			print ("Seems like you don't want to interact with any of the items. It's okay, that won't affect you at all in the future!!!!\n")
 
-print ("Welcome to hell!!!!\nWe are gonna have so much FUN!!!\n")	
+# Function to check and see what direction the user wants to go to.
+def answercheck():
+    answer = input("Where do you wanna go? ")
+    playerChoice.append(answer)
+    if answer.lower() == "rules":
+        rules()
+    elif answer.lower() == "up" or answer.lower() == "u" or answer.lower() == "right" or answer.lower() == "r":
+        room()
+    else:
+        print("I guess you missed the instructions!!!!")
+        print("You won't get a chance to fix this issue, so you can start from scratch.")
+
+
+# Function to check what item the user wants to interact with.
+def itemchecker():
+    obj = input("Which one of these items " + str(sample(roomObj, 2)) + " whould you like to interact with? ")
+    if obj.lower() == "weapon":
+        print(Weapon + "\n")
+    elif obj.lower() == "armor":
+        print(Armor + "\n")
+    elif obj.lower() == "potion":
+        print(Potion + "\n")
+        playerPotion.append(obj.lower())
+    elif obj.lower() == "book":
+        print(Book + "\n")
+        playerBook.append(obj.lower())
+    elif obj.lower() == "script":
+        print(Script + "\n")
+    else:
+        print("Seems like you don't want to interact with any of the items.")
+        print("It's okay, that won't affect you at all in the future!!!!")
+
+
+print("Welcome to hell!!!!\nWe are gonna have so much FUN!!!\n")
 rules()
