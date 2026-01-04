d702479be4b56155d379c30a386681a33b71376b
Kyvan Emami <emamikyvan@gmail.com>
2026-01-04 11:39:39 -0500

    added more functions and cleaned up a bit

--- input_validator.py
+++ input_validator.py
@@ -1,32 +1,59 @@
 #!/home/kyvan/python_env/bin/python
 
 
-# Class to validate input from users
-class InputValidator:
-    def is_integer(self, user_input):
-        try:
-            # Attempt to check if input to an integer
-            int(user_input)
-            print("It be a digit")
-            return True
-        except ValueError:
-            # If a ValueError is raised, the input is not a valid integer
-            print("Usage: Integer is expected")
+def is_integer(user_input):
+    if user_input.isdigit():
+        print(f"You entered the integer: {user_input}")
+        return True
+    else:
+        # If a ValueError is raised, the input is not a valid integer
+        print("Usage: Integer is expected")
+        return False
 
-    def is_string(self, user_input):
-        try:
-            # Attempt to check if input to a string
-            str(user_input)
-            print("It be a string")
-            return True
-        except ValueError:
-            print("Usage: String is expected")
 
-    def is_float(self, user_input):
+def is_string(user_input):
+    # Attempt to check if input is a string
+    if user_input.isalpha():
+        print(f"You entered the string: {user_input}")
+        return True
+    else:
+        print("Usage: String is expected")
+        return False
+
+
+def is_white_space(user_input):
+    if user_input.isspace():
+        print(f"You entered White Space(s): {user_input}")
+        return True
+    else:
+        print("Usage: White Space is expected")
+        return False
+
+
+def is_float(user_input):
+    if user_input.isdigit() or user_input.isalpha() or user_input.isspace():
+        print("Usage: Float is expected")
+        return True
+    else:
         try:
-            # Attempt to check if input to a float
             float(user_input)
-            print("It be a float")
-            return True
+            print(f"You entered the float: {user_input}")
         except ValueError:
             print("Usage: Float is expected")
+
+
+def is_float_no_output(user_input):
+    try:
+        float(user_input)
+        return True
+    except ValueError:
+        return None
+
+
+def is_special_character(user_input):
+    if user_input.isdigit() or user_input.isalpha() or user_input.isspace() or is_float_no_output(user_input):
+        print("Usage: Special Character is expected")
+        return True
+    else:
+        print(f"You entered the Special Character: {user_input}")
+        return True
