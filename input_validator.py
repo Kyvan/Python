d450a2ad7bd68fe6f6fbbdc86861995e78ff7fc6
Kyvan Emami <emamikyvan@gmail.com>
2026-01-08 21:24:19 -0500

    simplified and removed extra functions

--- input_validator.py
+++ input_validator.py
@@ -5,51 +5,20 @@ def is_integer(user_input):
     if user_input.isdigit():
         return True
     else:
-        # If a ValueError is raised, the input is not a valid integer
-        print("Usage: Integer is expected")
         return False
-
 
 def is_string(user_input):
     # Attempt to check if input is a string
     if user_input.isalpha():
         return True
     else:
-        print("Usage: String is expected")
-        return False
-
-
-def is_white_space(user_input):
-    if user_input.isspace():
-        return True
-    else:
-        print("Usage: White Space is expected")
         return False
 
 
 def is_float(user_input):
-    if user_input.isdigit():
-        print("Usage: Float is expected")
+    if is_integer(user_input):
         return False
-    else:
-        try:
-            float(user_input)
-            return True
-        except ValueError:
-            print("Usage: Float is expected")
-
-
-def is_float_no_output(user_input):
-    try:
-        float(user_input)
-        return True
-    except ValueError:
+    elif is_string(user_input):
         return False
-
-
-def is_special_character(user_input):
-    if user_input.isdigit() or user_input.isalpha() or user_input.isspace() or is_float_no_output(user_input):
-        print("Usage: Special Character is expected")
-        return True
     else:
         return True
